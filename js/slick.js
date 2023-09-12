69d006032f9bdaefcf3fac989731ed7eaca418e8
revanthreddy123 <ar.revanthreddy@gmail.com>
2023-09-12 20:26:12 +0530

    main1

---
+++ js/slick.js
@@ -0,0 +1,25 @@
+$('.owl-carousel').owlCarousel({
+  autoplay: true,
+  autoplayTimeout: 5000,
+  autoplayHoverPause: true,
+  loop: true,
+  margin: 50,
+  responsiveClass: true,
+  nav: true,
+  loop: true,
+  stagePadding: 30,
+  responsive: {
+    0: {
+      items: 1
+    },
+    568: {
+      items: 2
+    },
+    600: {
+      items: 3
+    },
+    1000: {
+      items:5
+    }
+  }
+})
